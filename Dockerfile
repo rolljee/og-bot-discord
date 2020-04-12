8a5930bfb0b3728a76339e03ca9bef2fc3887e23
Rolljee <n.riquelmebareiro@gmail.com>
2020-04-12 11:41:47 +0200

    deploy.sh

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM node:lts as build
 
+ARG DISCORD_TOKEN
+ENV DISCORD_TOKEN=${DISCORD_TOKEN}}
 WORKDIR /app
 COPY . ./
 RUN npm install
@@ -9,4 +11,4 @@ FROM node:lts as production
 WORKDIR /app
 COPY --from=build /app /app
 
-CMD ["npm", "start"]
+CMD ["$DISCORD_TOKEN", "npm", "start"]
--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ ssh -i ~/.ssh/id_rsa.pub $USER@$IP_ADDR
 cd ~/Code/og-bot-discord
 git pull origin master
 docker stop og-bot && docker rm og-bot
-docker build -t og-bot:latest && docker run --rm --name og-bot -d og-bot
+docker build -t og-bot:latest && docker run --rm --name og-bot -d og-bot --build-args DISCORD_TOKEN=$DISCORD_TOKEN
 
 set +e
